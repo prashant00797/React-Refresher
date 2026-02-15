27f046c6ec812a85c63377da7b615da14ba30b11
prashant00797 <prashantnath6307@gmail.com>
2026-02-15 22:33:36 +0530

    building the skeleton with the help of parcel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+import React from "react";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
+);
