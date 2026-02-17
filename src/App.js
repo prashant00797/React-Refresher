52a288f53f30ee11036d77202685baa17d1d9bc2
prashant00797 <prashantnath6307@gmail.com>
2026-02-17 17:00:21 +0530

    part 3 refresher see readme in commits for better understanding each time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+const Title = () => <h1>React Refresher</h1>;
+
 export const App = () => {
+  const random = Math.random().toFixed(1);
   return (
     <div>
-      <h1>Test</h1>
+      <h1>Test {random}</h1>
+      {Title()} {/* Ultimate its a js arrow function and we are invoking it */}
+      <Title></Title> {/* All this three syntax mean the same  */}
+      <Title />
     </div>
   );
 };
